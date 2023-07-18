a35876962376be11fc8c2ee435e53c30944df018
Martin Webber <2012faradey44347@gmail.com>
2023-07-18 10:57:38 +0300

    1.1

--- Cumulants/Cumulants.cpp
+++ Cumulants/Cumulants.cpp
@@ -11,94 +11,94 @@ std::complex <float> *Cumulants::getCumulants() {
 void Cumulants::setCumulants(std::complex <float>* cumulants) {
     this->cumulants = cumulants;
 }
-std::complex <float> Cumulants::epr(unsigned p, unsigned r, unsigned k, unsigned n, std::complex <float>* sPhase, std::complex <float>* sComp) {
+std::complex <float> Cumulants::epr(unsigned p, unsigned r, unsigned n, std::complex <float>* sPhase, std::complex <float>* sComp) {
     std::complex <float> result = 0;
-    for (unsigned i = 1; i <= k; i++)
+    for (unsigned i = 1; i <= n; i++)
         result += pow(sPhase[i], p) * pow(sComp[i], r);
     result /= n;
     return result;
 }
-std::complex <float> Cumulants::cpr(unsigned p, unsigned r, unsigned k, unsigned n, std::complex <float>* sPhase, std::complex <float>* sComp) {
+std::complex <float> Cumulants::cpr(unsigned p, unsigned r, unsigned n, std::complex <float>* sPhase, std::complex <float>* sComp) {
     std::complex <float> result = 0;
     if (p == 2 && (r == 0 || r == 1)) {
-        result = epr(p, r, k, n, sPhase, sComp);
+        result = epr(p, r, n, sPhase, sComp);
     }
     else if (p == 4 && r == 0) {
-        result = epr(4, 0, k, n, sPhase, sComp) - 3.0f * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 2);
+        result = epr(4, 0, n, sPhase, sComp) - 3.0f * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 2);
     }
     else if (p == 4 && r == 1) {
-        result = epr(4, 1, k, n, sPhase, sComp)
-            - 4.0f * epr(3, 0, k, n, sPhase, sComp) * epr(1, 1, k, n, sPhase, sComp)
-            - 6.0f * epr(2, 1, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp);
+        result = epr(4, 1, n, sPhase, sComp)
+            - 4.0f * epr(3, 0, n, sPhase, sComp) * epr(1, 1, n, sPhase, sComp)
+            - 6.0f * epr(2, 1, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp);
     }
     else if (p == 4 && r == 2) {
-        result = epr(4, 2, k, n, sPhase, sComp)
-            - epr(4, 0, k, n, sPhase, sComp) * epr(0, 2, k, n, sPhase, sComp)
-            - 8.0f * epr(3, 1, k, n, sPhase, sComp) * epr(1, 1, k, n, sPhase, sComp)
-            - 4.0f * epr(3, 0, k, n, sPhase, sComp) * epr(1, 2, k, n, sPhase, sComp)
-            - 6.0f * epr(2, 2, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp)
-            - 6.0f * (std::complex <float>) pow(epr(2, 1, k, n, sPhase, sComp), 2)
-            + 6.0f * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 2) * epr(0, 2, k, n, sPhase, sComp)
-            + 24.0f * epr(2, 0, k, n, sPhase, sComp) * (std::complex <float>) pow(epr(1, 1, k, n, sPhase, sComp), 2);
+        result = epr(4, 2, n, sPhase, sComp)
+            - epr(4, 0, n, sPhase, sComp) * epr(0, 2, n, sPhase, sComp)
+            - 8.0f * epr(3, 1, n, sPhase, sComp) * epr(1, 1, n, sPhase, sComp)
+            - 4.0f * epr(3, 0, n, sPhase, sComp) * epr(1, 2, n, sPhase, sComp)
+            - 6.0f * epr(2, 2, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp)
+            - 6.0f * (std::complex <float>) pow(epr(2, 1, n, sPhase, sComp), 2)
+            + 6.0f * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 2) * epr(0, 2, n, sPhase, sComp)
+            + 24.0f * epr(2, 0, n, sPhase, sComp) * (std::complex <float>) pow(epr(1, 1, n, sPhase, sComp), 2);
     }
     else if (p == 6 && r == 0) {
-        result = epr(6, 0, k, n, sPhase, sComp)
-            - 15.0f * epr(4, 0, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp)
-            - 10.0f * (std::complex <float>) pow(epr(3, 0, k, n, sPhase, sComp), 2)
-            + 30.0f * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 2) * epr(1, 1, k, n, sPhase, sComp);
+        result = epr(6, 0, n, sPhase, sComp)
+            - 15.0f * epr(4, 0, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp)
+            - 10.0f * (std::complex <float>) pow(epr(3, 0, n, sPhase, sComp), 2)
+            + 30.0f * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 2) * epr(1, 1, n, sPhase, sComp);
     }
     else if (p == 6 && r == 1) {
-        result = epr(6, 1, k, n, sPhase, sComp)
-            - 15.0f * epr(4, 1, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp)
-            - 6.0f * epr(5, 0, k, n, sPhase, sComp) * epr(1, 1, k, n, sPhase, sComp)
-            - 20.0f * epr(3, 1, k, n, sPhase, sComp) * epr(3, 0, k, n, sPhase, sComp)
-            - 15.0f * epr(4, 0, k, n, sPhase, sComp) * epr(2, 1, k, n, sPhase, sComp)
-            + 90.0f * epr(2, 1, k, n, sPhase, sComp) * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 2)
-            + 120.0f * epr(3, 0, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp) * epr(1, 1, k, n, sPhase, sComp);
+        result = epr(6, 1, n, sPhase, sComp)
+            - 15.0f * epr(4, 1, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp)
+            - 6.0f * epr(5, 0, n, sPhase, sComp) * epr(1, 1, n, sPhase, sComp)
+            - 20.0f * epr(3, 1, n, sPhase, sComp) * epr(3, 0, n, sPhase, sComp)
+            - 15.0f * epr(4, 0, n, sPhase, sComp) * epr(2, 1, n, sPhase, sComp)
+            + 90.0f * epr(2, 1, n, sPhase, sComp) * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 2)
+            + 120.0f * epr(3, 0, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp) * epr(1, 1, n, sPhase, sComp);
     }
     else if (p == 7 && r == 1) {
-        result = epr(7, 1, k, n, sPhase, sComp)
-            - 21.0f * epr(5, 1, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp)
-            - 7.0f * epr(1, 1, k, n, sPhase, sComp) * epr(6, 0, k, n, sPhase, sComp)
-            - 35.0f * epr(4, 1, k, n, sPhase, sComp) * epr(3, 0, k, n, sPhase, sComp)
-            - 21.0f * epr(2, 1, k, n, sPhase, sComp) * epr(5, 0, k, n, sPhase, sComp)
-            - 35.0f * epr(4, 0, k, n, sPhase, sComp) * epr(3, 1, k, n, sPhase, sComp)
-            + 210.0f * epr(4, 0, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp) * epr(1, 1, k, n, sPhase, sComp)
-            + 210.0f * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 2) * epr(3, 1, k, n, sPhase, sComp)
-            + 420.0f * epr(3, 0, k, n, sPhase, sComp) * epr(2, 1, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp)
-            + 140.0f * (std::complex <float>) pow(epr(3, 0, k, n, sPhase, sComp), 2) * epr(1, 1, k, n, sPhase, sComp)
-            - 630.0f * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 3) * epr(1, 1, k, n, sPhase, sComp);
+        result = epr(7, 1, n, sPhase, sComp)
+            - 21.0f * epr(5, 1, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp)
+            - 7.0f * epr(1, 1, n, sPhase, sComp) * epr(6, 0, n, sPhase, sComp)
+            - 35.0f * epr(4, 1, n, sPhase, sComp) * epr(3, 0, n, sPhase, sComp)
+            - 21.0f * epr(2, 1, n, sPhase, sComp) * epr(5, 0, n, sPhase, sComp)
+            - 35.0f * epr(4, 0, n, sPhase, sComp) * epr(3, 1, n, sPhase, sComp)
+            + 210.0f * epr(4, 0, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp) * epr(1, 1, n, sPhase, sComp)
+            + 210.0f * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 2) * epr(3, 1, n, sPhase, sComp)
+            + 420.0f * epr(3, 0, n, sPhase, sComp) * epr(2, 1, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp)
+            + 140.0f * (std::complex <float>) pow(epr(3, 0, n, sPhase, sComp), 2) * epr(1, 1, n, sPhase, sComp)
+            - 630.0f * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 3) * epr(1, 1, n, sPhase, sComp);
     }
     else if (p == 8 && r == 0) {
-        result = epr(8, 0, k, n, sPhase, sComp)
-            - 28.0f * epr(6, 0, k, n, sPhase, sComp) * epr(2, 0, k, n, sPhase, sComp)
-            - 56.0f * epr(5, 0, k, n, sPhase, sComp) * epr(3, 0, k, n, sPhase, sComp)
-            - 35.0f * (std::complex <float>) pow(epr(4, 0, k, n, sPhase, sComp), 2)
-            + 420.0f * epr(4, 0, k, n, sPhase, sComp) * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 2)
-            + 560.0f * (std::complex <float>) pow(epr(3, 0, k, n, sPhase, sComp), 2) * epr(2, 0, k, n, sPhase, sComp)
-            - 630.0f * (std::complex <float>) pow(epr(2, 0, k, n, sPhase, sComp), 4);
+        result = epr(8, 0, n, sPhase, sComp)
+            - 28.0f * epr(6, 0, n, sPhase, sComp) * epr(2, 0, n, sPhase, sComp)
+            - 56.0f * epr(5, 0, n, sPhase, sComp) * epr(3, 0, n, sPhase, sComp)
+            - 35.0f * (std::complex <float>) pow(epr(4, 0, n, sPhase, sComp), 2)
+            + 420.0f * epr(4, 0, n, sPhase, sComp) * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 2)
+            + 560.0f * (std::complex <float>) pow(epr(3, 0, n, sPhase, sComp), 2) * epr(2, 0, n, sPhase, sComp)
+            - 630.0f * (std::complex <float>) pow(epr(2, 0, n, sPhase, sComp), 4);
     }
     else std::cout << "Error";
     return result;
 }
-void Cumulants::cprAll(unsigned k, unsigned n, std::complex <float>* sPhase, std::complex <float>* sComp) {
-    std::complex <float> e20 = epr(2, 0, k, n, sPhase, sComp),
-        e21 = epr(2, 1, k, n, sPhase, sComp),
-        e40 = epr(4, 0, k, n, sPhase, sComp),
-        e41 = epr(4, 1, k, n, sPhase, sComp),
-        e30 = epr(3, 0, k, n, sPhase, sComp),
-        e11 = epr(1, 1, k, n, sPhase, sComp),
-        e42 = epr(4, 2, k, n, sPhase, sComp),
-        e02 = epr(0, 2, k, n, sPhase, sComp),
-        e31 = epr(3, 1, k, n, sPhase, sComp),
-        e12 = epr(1, 2, k, n, sPhase, sComp),
-        e22 = epr(2, 2, k, n, sPhase, sComp),
-        e60 = epr(6, 0, k, n, sPhase, sComp),
-        e61 = epr(6, 1, k, n, sPhase, sComp),
-        e50 = epr(5, 0, k, n, sPhase, sComp),
-        e71 = epr(7, 1, k, n, sPhase, sComp),
-        e51 = epr(5, 1, k, n, sPhase, sComp),
-        e80 = epr(8, 0, k, n, sPhase, sComp);
+void Cumulants::cprAll(unsigned unsigned n, std::complex <float>* sPhase, std::complex <float>* sComp) {
+    std::complex <float> e20 = epr(2, 0, n, sPhase, sComp),
+        e21 = epr(2, 1, n, sPhase, sComp),
+        e40 = epr(4, 0, n, sPhase, sComp),
+        e41 = epr(4, 1, n, sPhase, sComp),
+        e30 = epr(3, 0, n, sPhase, sComp),
+        e11 = epr(1, 1, n, sPhase, sComp),
+        e42 = epr(4, 2, n, sPhase, sComp),
+        e02 = epr(0, 2, n, sPhase, sComp),
+        e31 = epr(3, 1, n, sPhase, sComp),
+        e12 = epr(1, 2, n, sPhase, sComp),
+        e22 = epr(2, 2, n, sPhase, sComp),
+        e60 = epr(6, 0, n, sPhase, sComp),
+        e61 = epr(6, 1, n, sPhase, sComp),
+        e50 = epr(5, 0, n, sPhase, sComp),
+        e71 = epr(7, 1, n, sPhase, sComp),
+        e51 = epr(5, 1, n, sPhase, sComp),
+        e80 = epr(8, 0, n, sPhase, sComp);
     cumulants[0] = e20;
     cumulants[1] = e21;
     cumulants[2] = e40 - 3.0f * (std::complex <float>) pow(e20, 2);
--- Cumulants/Cumulants.h
+++ Cumulants/Cumulants.h
@@ -8,7 +8,7 @@ public:
     Cumulants(std::complex <float> *cumulants);
     std::complex <float> *getCumulants();
     void setCumulants(std::complex <float> *cumulants);
-    std::complex <float> epr(unsigned p, unsigned r, unsigned k, unsigned n, std::complex <float> *sPhase, std::complex <float> *sComp);
-    std::complex <float> cpr(unsigned p, unsigned r, unsigned k, unsigned n, std::complex <float> *sPhase, std::complex <float> *sComp);
-    void cprAll(unsigned k, unsigned n, std::complex <float> *sPhase, std::complex <float> *sComp);
+    std::complex <float> epr(unsigned p, unsigned r, unsigned n, std::complex <float> *sPhase, std::complex <float> *sComp);
+    std::complex <float> cpr(unsigned p, unsigned r, unsigned n, std::complex <float> *sPhase, std::complex <float> *sComp);
+    void cprAll(unsigned n, std::complex <float> *sPhase, std::complex <float> *sComp);
 };
